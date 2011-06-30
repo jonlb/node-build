f1329ed541aa4105410f79bfcca0fdd9fae487e8
Jonathan Bomgardner <jonlb@comcast.net>
2011-06-30 08:35:59 -0700

    added dependencies and other updates

---
+++ tasks/loader.js
@@ -0,0 +1,17 @@
+//require dependencies
+var jxLoader = require('loader/jxLoader').jxLoader;
+
+//the loader instance itself
+var loader = null;
+
+
+
+modules.exports.tasks = {
+    combine: function(options){
+        
+    },
+    
+    createDeps: function(options){
+        
+    }
+};
