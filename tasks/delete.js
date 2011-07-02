79070842cfca6a9ceaf1dec180c93132b6693b23
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-02 15:15:37 -0700

    changes to copyDir task

--- tasks/delete.js
+++ tasks/delete.js
@@ -1,13 +1,16 @@
 var util = require("util"),
     Promise = require("promise").Promise,
-    fs = require("fs");
+    fs = require("fs"),
+    path = require("path");
 
 module.exports.tasks = {
     deleteFile: function (options, config, logger) {
         var p = new Promise();
         Array.from(options).each(function(file){
             logger.info("Removing file: " + file);
-           fs.unlinkSync(fs.realpathSync(file)); 
+            if (path.exists(file)) {
+                fs.unlinkSync(file);
+            }
         });
         
         return p;
