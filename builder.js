be9607734ec8c280939b4857f4a5a09ae2d02940
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-01 20:58:06 -0700

    change to builder.js

--- builder.js
+++ builder.js
@@ -47,7 +47,7 @@ Builder.config = function(config, logfile){
         _logger.debug("config is nil");
     } else {
         _logger.debug("config is not nil");
-        _logger.info("config: ", { test: 'somes string'});
+        _logger.info("config: ", config);
     }
     
     _logger.debug("The passed in config");
