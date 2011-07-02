19523fdeab372af92c97288ebdbe0a41296a7416
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-02 15:02:01 -0700

    changes to copyDir task

--- tasks/copyDir.js
+++ tasks/copyDir.js
@@ -8,7 +8,7 @@ module.exports.tasks = {
         var p = new Promise();
         options.copy.each(function(dir){
             var from = fs.realpathSync(options.basedirs.from + "/" + dir),
-                to = fs.realpathSync(options.basedirs.to + "/" + dir);
+                to = options.basedirs.to + "/" + dir;   //may not exist already
             logger.info("Copying " + from + " to " + to);
             wrench.copyDirSyncRecursive(from, to);
         });
