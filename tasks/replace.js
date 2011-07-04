9a68338301978735cdf86865223a0364f53ff8e4
Jonathan Bomgardner <jonlb@comcast.net>
2011-07-03 20:50:52 -0700

    update to replace task

--- tasks/replace.js
+++ tasks/replace.js
@@ -8,10 +8,13 @@ module.exports.tasks = {
         
         Array.from(options).each(function(opt){
             logger.info("Replacing " + opt.token + " with " + opt.value + " in " + opt.file);
+            logger.info("\treading file");
             var original = fs.readFileSync(fs.realpathSync(opt.file), 'utf-8');
             var pattern = new RegExp("\\" + opt.token,"g");
+            logger.info("\treplacing token");
             var final = original.replace(pattern, opt.value);
-            fs.writeFileSync(fs.realpathSync(opt.target), final, 'utf-8');
+            logger.info("\tsaving file");
+            fs.writeFileSync(opt.target, final, 'utf-8');
         });
         
         p.resolve(true);
