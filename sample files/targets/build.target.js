0b8c4981d60e1329d826b2c91b70ce21d20736b4
Jonathan Bomgardner <jonlb@comcast.net>
2012-02-04 23:01:38 -0800

    Update sample files/targets/build.target.js

--- sample files/targets/build.target.js
+++ sample files/targets/build.target.js
@@ -1,11 +1,14 @@
 
-
+//passes in the config object and a logger object so you can reference config options
+//and do logging (the logger is an instance of the winston logger.
 module.exports = function(config,logger) {
   
+    //return an object with the info for this target
     return {
-        name: "build",
-        description: "Build the Jx library",
-        depends: "prepare",
+        name: "build",  //target name you reference it by
+        description: "Build the Jx library", //a short description of the target
+        depends: "prepare", //any target dependencies. use an array of strings if there is more than one.
+        //start your list of tasks here. this should be an array of objects.
         tasks: [{
             callTarget: {
                 target: "theme",
